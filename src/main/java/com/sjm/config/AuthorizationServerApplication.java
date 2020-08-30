a618fb227278d493db3dfbbcb1fb5f60fb406d24
jmshi <jmshi@LAPTOP-THMSSAL0>
2020-08-30 11:42:24 +0900

    init

---
+++ src/main/java/com/sjm/config/AuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package com.sjm.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = "com.sjm")
+public class AuthorizationServerApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sjm/config/ResourceServerWebConfig.java
@@ -0,0 +1,13 @@
+package com.sjm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "com.sjm.web.controller" })
+public class ResourceServerWebConfig implements WebMvcConfigurer {
+    //
+}
